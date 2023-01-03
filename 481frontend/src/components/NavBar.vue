fb17bf89576b3c4e88c911235a997796add2ca26
sktp06 <kantaporn_s@cmu.ac.th>
2023-01-03 20:36:15 +0700

    feat: implement login, sign up

---
+++ 481frontend/src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+  <nav
+    class="top-0 absolute z-50 w-full flex flex-wrap items-center justify-between px-2 py-3"
+  >
+    <div
+      class="container px-4 mx-auto flex flex-wrap items-center justify-between"
+    >
+      <div
+        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+      >
+        <router-link to="/">AnimeList</router-link>
+      </div>
+      <div
+        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+      >
+        <router-link to="/login">Login</router-link>
+        <div
+          class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+        >
+          <router-link to="/sign-up">Sign Up</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      showMenu: false,
+    };
+  },
+  methods: {
+    toggleNavbar: function () {
+      this.showMenu = !this.showMenu;
+    },
+  },
+};
+</script>
--- 481frontend/src/router/index.js
+++ 481frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import Login from "../views/LoginView.vue";
+import SignUp from "../views/SignUpView.vue";
 const routes = [
   {
     path: "/",
@@ -8,14 +9,15 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/login",
+    component: Login,
   },
+  { path: "/:pathMatch(.*)*", redirect: "/login" },
+  {
+    path: "/sign-up",
+    component: SignUp,
+  },
+  { path: "/:pathMatch(.*)*", redirect: "/sign-up" },
 ];
 
 const router = createRouter({
