e978b3cd5a9d351bb721365091acc177c479f9f5
sktp06 <kantaporn_s@cmu.ac.th>
2023-01-10 14:00:40 +0700

    feat: implement set up card

---
+++ 481frontend/src/views/EventCardView.vue
@@ -0,0 +1,12 @@
+<template>
+  <EventCard> ไอ่ป่าน </EventCard>
+</template>
+<script>
+import EventCard from "../components/EventCard.vue";
+export default {
+  name: "event-card",
+  components: {
+    EventCard,
+  },
+};
+</script>
